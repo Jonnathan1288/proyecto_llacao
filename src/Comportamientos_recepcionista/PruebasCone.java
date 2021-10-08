a2a3cbcd1c53098b50a5376fd172c8201c6fd73c
DELL <DELL@DESKTOP-9147F0D>
2021-10-07 23:27:13 -0500

    Clase pruebas

---
+++ src/Comportamientos_recepcionista/PruebasCone.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Comportamientos_recepcionista;
+
+/**
+ *
+ * @author DELL
+ */
+public class PruebasCone {
+    String nombre;
+
+    public PruebasCone() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
